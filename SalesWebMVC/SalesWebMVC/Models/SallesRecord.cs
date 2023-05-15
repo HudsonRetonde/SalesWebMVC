7d16119c5e5a94aaaad5d9f05bbdd2b0cec0a940
Hudson Retonde <hudsonsretonde@gmail.com>
2023-05-15 20:34:28 -0300

    Other entities and second migration

---
+++ SalesWebMVC/SalesWebMVC/Models/SallesRecord.cs
@@ -0,0 +1,27 @@
+﻿using SalesWebMVC.Models.Enums;
+using System;
+
+namespace SalesWebMVC.Models
+{
+	public class SallesRecord
+	{
+		public int Id { get; set; }
+		public DateTime Date { get; set; }
+		public double Amount { get; set; }
+		public SalesStatus Status { get; set; }
+		public Seller Seller  { get; set; }
+
+		public SallesRecord()
+		{
+		}
+
+		public SallesRecord(int id, DateTime date, double amount, SalesStatus status, Seller seller)
+		{
+			Id = id;
+			Date = date;
+			Amount = amount;
+			Status = status;
+			Seller = seller;
+		}
+	}
+}
